bb16f0b3e54d7b5b8bbe799484c0d84b5fd34fc5
jcarlos <carlosmaster10@gmail.com>
2016-10-07 16:09:26 -0300

    minor fixes

--- gitManager/src/gitmanager/GitManager.java
+++ gitManager/src/gitmanager/GitManager.java
@@ -21,11 +21,12 @@ import gitmanager.report.GeneralReport;
 
 public class GitManager {
     
-    private Iterable<RevCommit> commitList;
+    private Iterable<RevCommit> commitList = null;
     private Repository repository;
     private Git git;
     private Hashtable<String, ArrayList<String>> changeMap;
     private ArrayList<String> changeMapKeys;
+    private boolean changeMapGenerated = false;
 
     // remotePath; //Ex git@github.com:me/mytestrepo.git
     // localPath; //Ex /home/repos/...
@@ -71,10 +72,12 @@ public class GitManager {
     }
 
     public Iterable<RevCommit> getCommitList() {
-        try {
-            commitList = git.log().call();
-        } catch (Exception e) {
-            GeneralReport.getInstance().reportError("Não foi possível acessar commits do repositório");
+        if (this.commitList == null) {
+            try {
+                this.commitList = git.log().call();
+            } catch (Exception e) {
+                GeneralReport.getInstance().reportError("Não foi possível acessar commits do repositório");
+            }
         }
 
         return commitList;
@@ -82,15 +85,16 @@ public class GitManager {
 
     public ArrayList<String> getCommitHashList() {
         ArrayList<String> commitIds = new ArrayList<String>();
-        getCommitList();
-        for (RevCommit commit : commitList) {
+        for (RevCommit commit : this.getCommitList()) {
             commitIds.add(commit.getId().getName());
         }
         return commitIds;
     }
 
     public void generateChangedFilesMap() {
-        ;
+        if (this.changeMapGenerated == true)
+            return;
+        
         String commitId = "";
         try {
             RevWalk rw = new RevWalk(repository);
@@ -121,17 +125,16 @@ public class GitManager {
                     GeneralReport.getInstance()
                             .reportError("Não foi possível acessar as diferenças do commit " + commitId);
                 }
-
             }
             
             rw.close();
             df.close();
-
+            
         } catch (Exception e) {
             GeneralReport.getInstance()
                     .reportError("Não foi possível acessar as diferenças nos commits do repositório");
         }
-
+        this.changeMapGenerated = true;
     }
 
     public Hashtable<String, ArrayList<String>> getChangedFilesMap() {
@@ -140,6 +143,7 @@ public class GitManager {
     }
 
     public ArrayList<String> getChangeMapKeys() {
+        generateChangedFilesMap();
         return changeMapKeys;
     }
 
