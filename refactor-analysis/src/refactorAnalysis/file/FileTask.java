477cca9ce136d0d2f381a131cf6afb6b5897dc30
jcarlos <carlosmaster10@gmail.com>
2016-10-13 10:57:55 -0300

    space to tab

--- refactor-analysis/src/refactorAnalysis/file/FileTask.java
+++ refactor-analysis/src/refactorAnalysis/file/FileTask.java
@@ -3,21 +3,21 @@ package refactorAnalysis.file;
 import refactorAnalysis.git.GitProject;
 
 public class FileTask implements Runnable {
-    
-    private String filePath;
-    private String commitHash;
-    private GitProject gitProject;
 
-    public FileTask(GitProject gitProject, String commitHash, String filePath) {
-        this.filePath = filePath;
-        this.commitHash = commitHash;
-        this.gitProject = gitProject;
-    }
+	private String filePath;
+	private String commitHash;
+	private GitProject gitProject;
 
-    @Override
-    public void run() {
-        // TODO Auto-generated method stub
-        
-    }
+	public FileTask(GitProject gitProject, String commitHash, String filePath) {
+		this.filePath = filePath;
+		this.commitHash = commitHash;
+		this.gitProject = gitProject;
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
