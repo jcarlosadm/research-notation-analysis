e8d3a7abd259f26c2c4d7c889c7c5918da276d29
jcarlos <carlosmaster10@gmail.com>
2016-10-13 23:56:37 -0300

    FunctionAnalyzer documented

--- refactor-analysis/src/refactorAnalysis/file/function/FunctionAnalyzer.java
+++ refactor-analysis/src/refactorAnalysis/file/function/FunctionAnalyzer.java
@@ -5,8 +5,10 @@ import java.io.File;
 import refactorAnalysis.folderManager.FolderManager;
 
 public class FunctionAnalyzer {
-	private File folder;
 	
+	// master folder
+	private File folder;
+
 	public FunctionAnalyzer(String projectName, String currentCommitHash) {
 		String folderPath = FolderManager.createTempProjectFolder(projectName) + File.separator + currentCommitHash;
 		this.folder = new File(folderPath);
@@ -14,8 +16,19 @@ public class FunctionAnalyzer {
 			this.folder.mkdirs();
 		}
 	}
-	
-	public File[] getFunctionFiles(File file, String subfolderPath) {
+
+	/**
+	 * Get a array of files, where each file gets a function. the file name must
+	 * be the same of function name, i.e., a function with name "run" turns into
+	 * a file with name "run.c". The file must be ended with ".c" extension
+	 * 
+	 * @param file
+	 *            file with all functions
+	 * @param subfolderPath
+	 *            subfolder where will put the files.
+	 * @return array of files
+	 */
+	public File[] getFunctionFiles(File file, String subfolder) {
 		// TODO implement
 		return null;
 	}
